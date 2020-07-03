6a6ac777cfb46fe9a4781e5a74446784c017d4dd
Nacho Betancourt <trsgarp@gmail.com>
2020-07-03 15:40:43 -0500

    Papa updated to obtain address list from csv and display them with a checkbox pointing to geocode (pending)

--- src/App.js
+++ src/App.js
@@ -5,50 +5,10 @@ function App() {
 
   const [addressList, setAddressList] = useState([]);
 
-  // function initMap() {
-  //   const map = new google.maps.Map(document.getElementById('map'), {
-  //     zoom: 11,
-  //     center: {lat: 19.3910038, lng: -99.2836963},
-  //   });
-  //   const geocoder = new google.maps.Geocoder();
-  //   const upload = document.getElementById('upload')
-  //   upload.addEventListener('change', () => {
-  //     changeFile(geocoder, map);
-  //   });
-  // }
-
-  // async function geocodeAddress(geocoder, map, address) {
-  //   geocoder.geocode({'address': address}, (results, status) => {
-  //     if (status === 'OK') {
-  //       map.setCenter(results[0].geometry.location);
-  //       const marker = new google.maps.Marker({
-  //         map: map,
-  //         position: results[0].geometry.location
-  //       });
-  //     } else {
-  //       console.log('Geocode was not successful for the following reason: ' + status);
-  //     }
-  //   });
-  // }
-
-  // const changeFile = (e) => {
-  //   console.log('e', e)
-  //   const file = document.getElementById("upload");
-  //   if (file) {
-  //     const csv = file.files[0];
-  //     const json = Papa.parse(csv, {
-  //       header: true,
-  //       skipEmptyLines: true,
-  //       complete: async results => {
-  //         const minResults = results.data.slice(0, 10);
-  //         const resultAdresses = minResults.map(result => {
-  //           const address = `${result.StreetName} ${result.StreetNumber}, ${result.Neighborhood}, ${result.ZipCode}`;
-  //         })
-  //         setAddressList(resultAdresses);
-  //       }
-  //     });
-  //   }
-  // }
+  const getGeocode = address => {
+    console.log('address', address);
+    // PENDING TO INTEGRATE GOOGLE WITH REACT
+  }
 
   return (
     <div className="App">
@@ -56,8 +16,33 @@ function App() {
       <p>
         Upload your CSV file to obtain the logic routes for your address
       </p>
-      <Papa />
-      <div id="map"></div>
+      <div className="container">
+        <Papa setAddressList={setAddressList} />
+      </div>
+      <div className="container">
+        <div id="addreses">
+          {addressList.map(result => (
+            <div key={result.id} style={{marginTop: '1rem'}}>
+              <span>
+                <input
+                  type="checkbox"
+                  onChange={() => getGeocode(result.address)}
+                />
+              </span>
+              <span style={{marginLeft: '1rem'}}>
+                {result.id}
+              </span>
+              <span style={{margin: '0rem 1rem'}} >
+                -
+              </span>
+              <span>
+                {result.address}
+              </span>
+            </div>
+          ))}
+        </div>
+        <div id="map"></div>
+      </div>
     </div>
   );
 }
--- src/Components/Papa.js
+++ src/Components/Papa.js
@@ -1,31 +1,37 @@
 import React from 'react'
 import { CSVReader } from 'react-papaparse'
 
-export default function CSV() {
+export default function CSV(props) {
+  const { setAddressList } = props;
 
   const handleOnDrop = (data) => {
-    console.log('---------------------------')
-    console.log(data)
-    console.log('---------------------------')
+    const results = data.slice(1,10).map(d => d.data);
+    const addresses = results.map(result => {
+      return {
+        id: result.ShipmentID,
+        address: `${result.StreetName} ${result.StreetNumber}, ${result.Neighborhood}, ${result.ZipCode}`,
+      }
+    });
+    setAddressList(addresses);
   }
 
   const handleOnError = (err, file, inputElem, reason) => {
-    console.log(err)
+    setAddressList([]);
+    alert(err);
   }
 
   const handleOnRemoveFile = (data) => {
-    console.log('---------------------------')
-    console.log(data)
-    console.log('---------------------------')
+    setAddressList([]);
   }
 
   return (
     <CSVReader
       onDrop={handleOnDrop}
       onError={handleOnError}
+      onRemoveFile={handleOnRemoveFile}
       addRemoveButton
       removeButtonColor='#659cef'
-      onRemoveFile={handleOnRemoveFile}
+      config={{header: true}}
     >
       <span>Drop CSV file here or click to upload.</span>
     </CSVReader>
